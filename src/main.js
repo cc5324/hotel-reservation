3efc3ed1a9c7dc164ca97ffa8ca4c6f072fcee2b
angela <awander5324@gmail.com>
2022-10-24 15:12:01 +0800

    feat(icon): better icon usage system - import font-awesome plugin - import svg sources with svgIcon component

--- src/main.js
+++ src/main.js
@@ -1,16 +1,26 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-// import pinia from "@/stores/store.js";
 
 import App from "./App.vue";
 import router from "./router";
 
-import "./assets/main.css";
+//引入 svg 雪碧圖
+import "virtual:svg-icons-register";
+import SvgIcon from "./components/plugins/SvgIcon.vue";
+
+//引入 font awesome
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import "@/plugins/fontawesome";
+
+// import "./assets/main.css";
+import "./assets/scss/base.scss";
 
 const app = createApp(App);
 
 // app.use(pinia);
 app.use(createPinia());
 app.use(router);
+app.component("SvgIcon", SvgIcon);
 
+app.component("font-awesome-icon", FontAwesomeIcon);
 app.mount("#app");
---
+++ src/plugins/fontawesome.js
@@ -0,0 +1,40 @@
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+// import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+/* add icons to the library */
+// library.add(faUserSecret);
+
+import {
+  faWifi,
+  faTv,
+  faMountain,
+  faUtensils,
+  faWind,
+  faBanSmoking,
+  faSnowflake,
+  faBaby,
+  faCouch,
+  faBellConcierge,
+  faMartiniGlass,
+  faDog,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+  faWifi,
+  faTv,
+  faMountain,
+  faUtensils,
+  faWind,
+  faBanSmoking,
+  faSnowflake,
+  faBaby,
+  faCouch,
+  faBellConcierge,
+  faMartiniGlass,
+  faDog
+);
