5c1badf5f64ed6a552b5f594287d86c4cc6f9e9b
angela <awander5324@gmail.com>
2022-11-04 22:19:42 +0800

    feat(stripe-separator): add stripe-separator

---
+++ src/components/StripeSeperator.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="stripes">
+    <div class="stripe"></div>
+    <div class="stripe"></div>
+    <div class="stripe"></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.stripes {
+  width: fit-content;
+  // outline: 1px solid #000;
+}
+.stripe {
+  display: inline-block;
+  width: 9px;
+  height: 1.5px;
+  background: #575757;
+  transform: rotate(45deg);
+
+  & + & {
+    margin-left: 5px;
+  }
+}
+</style>
