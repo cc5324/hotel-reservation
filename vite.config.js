d82c6927451901e793610298e3552b1346de6c9d
angela <awander5324@gmail.com>
2023-04-03 03:51:47 +0800

    feat(vite): add base to vite.config for deploy

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import path from "path";
 import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/hotel-reservation/",
   plugins: [
     vue({
       template: transformAssetUrls,
