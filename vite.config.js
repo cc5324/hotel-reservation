1ee683ce101f657d39236534bb6586e9b5274481
angela <awander5324@gmail.com>
2023-04-05 00:20:40 +0800

    feat(deploy): add redirects for netlify deploy

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import path from "path";
 import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/hotel-reservation/",
+  // base: "/hotel-reservation/",
   plugins: [
     vue({
       template: transformAssetUrls,
